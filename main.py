83820393aa7b89719baeaf119f35a9ecb2dffe41
SpikyLlama <62197004+SpikyLlama@users.noreply.github.com>
2021-05-06 12:16:49 -0700

    another update!! adds color changing possibilities

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 import tkinter
 from tkinter import messagebox
-from tkinter import filedialog
 from tkinter.filedialog import askopenfilename
+from tkinter.colorchooser import askcolor
 import eyed3
+import configparser
 
 root = tkinter.Tk()
-canvas = tkinter.Canvas(root, width=400, height=100)
+canvas = tkinter.Canvas(root, width=400, height=0)
+root.resizable(0,0)
 audio_file = None
 root.iconbitmap('src/icon32.ico')
 root.title('GUID3')
 file_chosen = False
+settings = configparser.ConfigParser()
+settings_file = settings.read('src/settings.ini')
+window_color = settings['window_color']['window_hex']
+
+
+def change_bg_color():
+    color = askcolor(title="Tkinter Color Chooser")
+    root.configure(bg=color[1])
+    file_choose_label.config(bg=color[1])
+    artist_label.config(bg=color[1])
+    title_label.config(bg=color[1])
+    album_label.config(bg=color[1])
+    track_label.config(bg=color[1])
+    color_string = ''.join(color[1])
+    settings.set('window_color', 'window_hex', color_string)
+    with open('src/settings.ini', 'w') as configfile:
+        settings.write(configfile)
+    return color
+
+
+change_bg_color_button = tkinter.Button(root, text='Select a Color', command=change_bg_color)
 
 
 def browse_files():
@@ -34,6 +57,7 @@ def change_artist():
         audio_file.tag.artist = artist_name.get()
         audio_file.tag.save()
 
+
 def change_title():
     if title_name.get() == '':
         return
@@ -41,12 +65,14 @@ def change_title():
         audio_file.tag.title = title_name.get()
         audio_file.tag.save()
 
+
 def change_album():
     if album_name.get() == '':
         return
     else:
         audio_file.tag.album = album_name.get()
         audio_file.tag.save()
+
 
 def change_track():
     try:
@@ -103,6 +129,7 @@ def print_track():
         if str(a) == "'NoneType' object has no attribute 'tag'":
             messagebox.showerror(root, "Select a file before reading tags.")
 
+
 def read_all():
     if file_chosen != True:
         messagebox.showerror(root, "Select a file before reading tags.")
@@ -133,10 +160,26 @@ track_label = tkinter.Label(root, text="Track #")
 track_name = tkinter.StringVar()
 track_entry = tkinter.Entry(root, textvariable=track_name)
 
+
+def change_bg_color_startup():
+    root.configure(bg=window_color)
+    file_choose_label.config(bg=window_color)
+    artist_label.config(bg=window_color)
+    title_label.config(bg=window_color)
+    album_label.config(bg=window_color)
+    track_label.config(bg=window_color)
+    settings['window_color']
+
+
+if __name__ == "__main__":
+    change_bg_color_startup()
+
+
 artist_entry.config(state='disabled')
 title_entry.config(state='disabled')
 album_entry.config(state='disabled')
 track_entry.config(state='disabled')
+
 
 def all_funcs():
     change_artist()
@@ -148,7 +191,11 @@ def all_funcs():
 apply_button = tkinter.Button(root, text="Apply", command=all_funcs)
 read_tags = tkinter.Button(root, text="Read tags...", command=read_all)
 
-file_choose_label.grid()
+with open('src/settings.ini', 'w') as configfile:
+    settings.write(configfile)
+
+change_bg_color_button.grid(sticky=tkinter.N+tkinter.E, row=2)
+file_choose_label.grid(row=2)
 file_choose.grid()
 artist_label.grid()
 artist_entry.grid()
